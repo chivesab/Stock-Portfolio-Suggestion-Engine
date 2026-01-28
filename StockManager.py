da63c5a396005aef76ce10e5d6661b98a8472606
chivesab <chivesab@gmail.com>
2026-01-28 12:04:22 +0900

    refactor: modernize project structure and tooling

--- StockManager.py
+++ StockManager.py
@@ -1,14 +1,13 @@
-import requests
 from datetime import datetime, timedelta
-from collections import defaultdict
-import json
+
+import requests
 
 stocks = {
-    'Ethical Investing': ["AAPL", "TSLA", "ADBE"],
-    'Growth Investing': ["ABNB", "ASAN", "AMD"],
-    'Index Investing': ["VOO", "VTI", "ARKK"],
-    'Quality Investing': ["NVDA", "TWLO", "CSCO"],
-    'Value Investing': ["INTC", "DIS", "CHWY"]
+    "Ethical Investing": ["AAPL", "TSLA", "ADBE"],
+    "Growth Investing": ["ABNB", "ASAN", "AMD"],
+    "Index Investing": ["VOO", "VTI", "ARKK"],
+    "Quality Investing": ["NVDA", "TWLO", "CSCO"],
+    "Value Investing": ["INTC", "DIS", "CHWY"],
 }
 
 
@@ -25,7 +24,10 @@ def get_latest_price(strategies):
             Exception("API Error")
         response_json = response.json()
         item = response_json[0]
-        latest_price[item["symbol"]] = {"price": item["price"], "strategy": get_strategy_by_stock(item["symbol"])}
+        latest_price[item["symbol"]] = {
+            "price": item["price"],
+            "strategy": get_strategy_by_stock(item["symbol"]),
+        }
     return latest_price
 
 
@@ -36,14 +38,30 @@ def allocate_stocks(amount, strategies):
 
     latest_price = get_latest_price(strategies)
 
-    latest_price = {k: v for k, v in sorted(latest_price.items(), key=lambda item: item[1]["price"], reverse=True)}
-    stock_amount1, stock_amount2, stock_amount3 = amount * 0.5, amount * 0.3, amount * 0.2
+    latest_price = {
+        k: v
+        for k, v in sorted(
+            latest_price.items(), key=lambda item: item[1]["price"], reverse=True
+        )
+    }
+    stock_amount1, stock_amount2, stock_amount3 = (
+        amount * 0.5,
+        amount * 0.3,
+        amount * 0.2,
+    )
     cnt = 0
     stock_amount = [stock_amount1, stock_amount2, stock_amount3]
     for ticker, meta in latest_price.items():
         stock_price = float(meta.get("price"))
-        number_of_stocks = round(((stock_amount[cnt])/stock_price), 2)
-        allocation.append({"name": ticker, "stocks": number_of_stocks, "price": stock_price, "strategy": meta.get("strategy")})
+        number_of_stocks = round(((stock_amount[cnt]) / stock_price), 2)
+        allocation.append(
+            {
+                "name": ticker,
+                "stocks": number_of_stocks,
+                "price": stock_price,
+                "strategy": meta.get("strategy"),
+            }
+        )
         value = round((number_of_stocks * stock_price), 2)
         pie_chart_data.append({"name": ticker, "value": value})
         cnt += 1
@@ -57,21 +75,30 @@ def allocate_stocks(amount, strategies):
         price_trend = []
         for h in stock_trend["historical"]:
             price_trend.append(h["close"])
-        weekly_trend_by_stock.append({"name" : symbol, "trend" : price_trend})
+        weekly_trend_by_stock.append({"name": symbol, "trend": price_trend})
     new_weekly_trend_by_stock = []
     today = datetime.today()
-    dates = [today + timedelta(days = i) for i in range(today.weekday() - 12, today.weekday() - 5)]
+    dates = [
+        today + timedelta(days=i)
+        for i in range(today.weekday() - 12, today.weekday() - 5)
+    ]
     for date in dates:
         temp_dict = {}
-        temp_dict['date'] = str(date.month) + '-' + str(date.day)
+        temp_dict["date"] = str(date.month) + "-" + str(date.day)
         new_weekly_trend_by_stock.append(temp_dict)
     j = -1
     for d in new_weekly_trend_by_stock:
         for i in range(len(weekly_trend_by_stock)):
-            d[weekly_trend_by_stock[i]['name']] = weekly_trend_by_stock[i]['trend'][j]
+            d[weekly_trend_by_stock[i]["name"]] = weekly_trend_by_stock[i]["trend"][j]
         j -= 1
 
-    return {"allocation": allocation, "weekly_trend": weekly_trend, "pie_chart_data": pie_chart_data, "weekly_trend_by_stock":new_weekly_trend_by_stock}
+    return {
+        "allocation": allocation,
+        "weekly_trend": weekly_trend,
+        "pie_chart_data": pie_chart_data,
+        "weekly_trend_by_stock": new_weekly_trend_by_stock,
+    }
+
 
 def get_total_weekly_trend(strategies):
     total_weekly_trend = get_weekly_trend(strategies)
@@ -82,17 +109,19 @@ def get_total_weekly_trend(strategies):
         for h in stock_trend["historical"]:
             price_trend.append(h["close"])
         total_weekly_trend_by_stock[symbol] = price_trend
-    return {"total_weekly_trend_by_stock":total_weekly_trend_by_stock}
-
-
+    return {"total_weekly_trend_by_stock": total_weekly_trend_by_stock}
 
 
 def get_weekly_trend(strategies):
     weekly_trend = {"total": []}
-    base_url = 'https://financialmodelingprep.com/api/v3/historical-price-full/'
+    base_url = "https://financialmodelingprep.com/api/v3/historical-price-full/"
     for strategy in strategies:
         for ticker in stocks.get(strategy):
-            url = base_url + ticker + "?timeseries=7&apikey=5e46a24c274b0a3f3a27f46f34bf0bb8"
+            url = (
+                base_url
+                + ticker
+                + "?timeseries=7&apikey=5e46a24c274b0a3f3a27f46f34bf0bb8"
+            )
             response = requests.get(url)
             if response.status_code != 200:
                 Exception("API Error")
@@ -100,8 +129,8 @@ def get_weekly_trend(strategies):
             weekly_trend["total"].append(response_json)
     return weekly_trend
 
+
 def get_strategy_by_stock(ticker):
     for strategy, tickers in stocks.items():
         if ticker in tickers:
             return strategy
-
--- app.py
+++ app.py
@@ -1,54 +1,62 @@
-from flask import Flask, jsonify, request, render_template
-import requests
 import json
-import StockManager
-from flask_cors import CORS, cross_origin
 
+from flask import Flask, render_template, request
+from flask_cors import CORS
+
+import StockManager
 
 app = Flask(__name__)
 CORS(app)
-app.config['CORS_HEADERS'] = 'Content-Type'
+app.config["CORS_HEADERS"] = "Content-Type"
 
-@app.route('/')
+
+@app.route("/")
 def index():
     return "Hello World!"
 
-@app.route('/suggest', methods = ['POST'])
+
+@app.route("/suggest", methods=["POST"])
 def suggest_stocks():
-    amount = request.json['amount']
+    amount = request.json["amount"]
     if amount < 5000:
         return json.dumps({"error": "Amount should be more than $5000"}), 500
 
-    strategies = request.json['strategies']
+    strategies = request.json["strategies"]
     if len(strategies) == 0:
         return json.dumps({"error": "Select at-least one strategy"}), 500
     if len(strategies) > 2:
-        return json.dumps({"error": "Maximum two strategies can be selected together"}), 500
+        return (
+            json.dumps({"error": "Maximum two strategies can be selected together"}),
+            500,
+        )
 
     allocations = StockManager.allocate_stocks(amount, strategies)
-    
+
     return json.dumps(allocations), 200
 
 
-@app.route('/weeklytrend', methods = ["GET"])
+@app.route("/weeklytrend", methods=["GET"])
 def get_total_weekly_trend():
-    total_strategies = ["Ethical Investing", "Growth Investing", "Index Investing", "Quality Investing", "Value Investing"]
+    total_strategies = [
+        "Ethical Investing",
+        "Growth Investing",
+        "Index Investing",
+        "Quality Investing",
+        "Value Investing",
+    ]
     trend = StockManager.get_total_weekly_trend(total_strategies)
     return json.dumps(trend), 200
 
 
 @app.route("/")
 def serve_template():
-    return render_template('index.html')
-
+    return render_template("index.html")
 
 
 @app.route("/dashboard")
 def serve_template_dashboard():
-    return render_template('index.html')
-
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
-    app.run(host='127.0.0.1', port = 5000, debug=True)
-    
+    app.run(host="127.0.0.1", port=5000, debug=True)
